bce52e7ac01600f67b18ea6be147e01dcf505496
zoe <wangzhao602010@pwrd.com>
2022-10-31 16:36:02 +0800

    msg

--- src/main/java/com/zoe/App.java
+++ src/main/java/com/zoe/App.java
@@ -14,7 +14,5 @@ public class App {
         System.out.println(123);
         System.out.println(123);
         System.out.println(123);
-        System.out.println(123);
-        System.out.println(123);
     }
 }
